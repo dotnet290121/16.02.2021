080c02973f1bfbf3db17acc563c895a93b7b4860
dotnet290121 <78213874+dotnet290121@users.noreply.github.com>
2021-02-16 21:00:26 +0200

    Add files via upload

---
+++ Log4Net/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Log4netMssql
+{
+    class Program
+    {
+        private static readonly log4net.ILog my_logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        static void Main(string[] args)
+        {
+            my_logger.Info("******************** System startup");
+            my_logger.Debug("This is a debug message");
+            my_logger.Warn("This is a warning message");
+            my_logger.Error("This is an ERROR message");
+            my_logger.Fatal("This is a FATAL message");
+            my_logger.Info("******************** System shutdown");
+
+        }
+    }
+}
---
+++ Log4Net/README.md
@@ -0,0 +1,4 @@
+# Download nugget: log4net
+# Add this line into AssemblyInfo.cs (last line):
+# [assembly: log4net.Config.XmlConfigurator()]
+
